75c8687b4493d410b242823ec20c44a3832fe3a9
drexduarte <drexduarte@gmail.com>
2017-11-30 17:17:15 -0200

    Adding Initial Components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,23 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { OrderListComponent } from './order-list/order-list.component';
+import { OrderEditComponent } from './order-list/order-edit/order-edit.component';
+import { HeaderComponent } from './header/header.component';
+import { CustomerListComponent } from './customer-list/customer-list.component';
+import { CustomerEditComponent } from './customer-list/customer-edit/customer-edit.component';
+import { OrderItemComponent } from './order-list/order-item/order-item.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OrderListComponent,
+    OrderEditComponent,
+    HeaderComponent,
+    CustomerListComponent,
+    CustomerEditComponent,
+    OrderItemComponent
   ],
   imports: [
     BrowserModule
